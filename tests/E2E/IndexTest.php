eb808227b0f81ecc7c8d88a781517b5957a67f89
Lauris Stekels <l.stekels@gmail.com>
2024-04-07 22:58:56 +0300

    test: demographic test

--- tests/E2E/IndexTest.php
+++ tests/E2E/IndexTest.php
@@ -11,8 +11,15 @@ class IndexTest extends PantherTestCase
     public function testIndex(): void
     {
         $client = static::createPantherClient(); // Your app is automatically started using the built-in web server
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
 
         self::assertPageTitleContains('Maģistra darbs');
+
+        // Click on the button/link. Replace 'button-id' with the id or text of your button
+        $link = $crawler->selectLink('Sākt')->link();
+        $client->click($link);
+
+        // Assert the current URL or page content to verify the redirection
+        $this->assertSame('http://127.0.0.1:9080/demographic', $client->getCurrentURL());
     }
 }
